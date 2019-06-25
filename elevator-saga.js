d7650257ad9f0efbf60953a232a1a582a58f4ee8
Carolina Martínez Pérez de Arce <cmartinezp.00@gmail.com>
2019-06-25 12:28:27 -0400

    minor changes

--- elevator-saga.js
+++ elevator-saga.js
@@ -5,22 +5,25 @@
 
         elevators.forEach(elevator => {
 
-            for (i = 0; i < qFloors; i++) {
-                    
-                elevator.goToFloor(i);
-            }
-
             elevator.on("idle", function() { 
                 for (i = 0; i < qFloors; i++) {
-                    
-                    elevator.goToFloor(Math.random() * (qFloors - 0) + 0);
+                    elevator.goToFloor(i);
                 }
             });
+
+            if(elevator.getPressedFloors().length > 0) {
+                var pressedFloors = elevator.getPressedFloors().sort();
+                pressedFloors.forEach(floor => {
+                    elevator.goToFloor(floor)
+                })
+                
+            }
 
         });
 
        
     },
+    
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
     }
